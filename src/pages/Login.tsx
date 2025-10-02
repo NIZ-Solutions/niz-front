89661c92ce5a4019242fd751b4d3fa6bcdd4c477
ParkMi-Hyun <parkmihyunn@gmail.com>
2025-10-02 18:15:41 +0900

    💄 : MyPage UI

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -42,7 +42,7 @@ export default function Login() {
   return (
     <>
       <div className="flex w-full flex-col items-center px-7">
-        <div className="flex h-screen min-h-fit w-full min-w-[280px] max-w-screen-lg flex-col items-center justify-center gap-12 self-center pt-[60px] md:flex-row md:justify-between lg:px-0">
+        <div className="flex h-screen min-h-fit w-full min-w-[280px] max-w-screen-lg flex-col items-center justify-center gap-12 self-center pt-[60px] md:flex-row md:justify-between md:text-center">
           {/* 로그인 타이틀 */}
           <h1 className="mr-auto text-4xl font-extrabold leading-[50px] md:min-w-[50%]">
             로그인
